a0614e83ed824746c178fc555aafe68dfe601d06
Mike Harris <mharris717@gmail.com>
2013-08-30 11:43:20 -0400

    Initial State

---
+++ config/environment.rb
@@ -0,0 +1,5 @@
+# Load the rails application
+require File.expand_path('../application', __FILE__)
+
+# Initialize the rails application
+EarTest3::Application.initialize!
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+EarTest3::Application.config.secret_token = '0ac36f7240f994accd8d69c4dfa690a4f9b8baf975b7b1b579df6e4c7df3fec10d73d2bfff55b021f5b37427327d46c8b0616dbb43c3ba5e8390d5d1acc7c6ca'
